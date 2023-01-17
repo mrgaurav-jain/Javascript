547458afc9a5d335674867c2e0d9282a54bb0d74
Gaurav Jain <gaurav.j@dpworld.com>
2023-01-17 15:57:15 +0530

    Variable and its type

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ console.log("Hello World, from new location");
 //Meaningful
 //case sensitie
 //cannot use systed defined keywords in code like if, else, 
+
+let name ='gaurav';//string
+//let is used to defined the variable, however is const is used for permanent assignment
+
+    //Primitive types
+            //string, boolean, number, undefined
+let age=30;
+let isapproved=false;//boolean
+let false=undefined;
+let color=null; 
+
+//Dynamic Typing type of variables can be changed in future
